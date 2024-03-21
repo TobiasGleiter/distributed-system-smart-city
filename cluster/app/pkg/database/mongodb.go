60506746aaf907e1d60e97899b5932bee6395862
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-03-21 10:39:50 +0100

    refactor

--- cluster/app/pkg/database/mongodb.go
+++
@@ -1,9 +0,0 @@
-package database
-
-import (
-    "smartcity/server/config"
-
-	"go.mongodb.org/mongo-driver/mongo"
-)
-
-var sensorsCollection *mongo.Collection = config.GetCollection(config.DB, "sensors")
--- server/airquality/models/airquality.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type AirQualityMessage struct {
-    SensorID string  `json:"sensor_id"`
-    Value    float64 `json:"value"`
-    Unit     string  `json:"unit"`
-}
